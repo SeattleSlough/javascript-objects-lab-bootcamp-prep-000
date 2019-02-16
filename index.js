f550cefd06051a341bc3aa4788453d7078bef94e
Michael Maggs <michaelmaggs@gmail.com>
2019-02-16 23:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign ({}, object, {[key] : value})
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+/*function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
-
-//var object = {foo: 'bar'};
-
-//var newObj = Object.assign({}, object)
-
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
+*/
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object)
+  newObject[key] = value
+  return newObject
 }
+  
